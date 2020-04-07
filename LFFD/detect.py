771f717376d17c5b7da01310a2fd308f1656b07b
Hoang V Do <hoangdv16@viettel.com.vn>
2020-04-07 16:11:44 +0700

    Update

--- LFFD/detect.py
+++ LFFD/detect.py
@@ -1,31 +1,59 @@
 import os
+import cv2
 import time
 import mxnet as mx
 from . import predict
 from .config_farm import configuration_10_320_20L_5scales_v2 as cfg
+from threading import Thread
+
+model_file_path = "LFFD/models/train_10_320_20L_5scales_v2_iter_1000000.params" 
+symbol_file_path = "LFFD/symbols/symbol_10_320_20L_5scales_v2_deploy.json"
+ctx = mx.gpu(0)
+
+### Threshold for Face Matching
+cosine_threshold = 0.8
+proba_threshold = 0.6
+comparing_num = 5
+face_predictor = predict.Predict(mxnet=mx,
+                    symbol_file_path=symbol_file_path,
+                    model_file_path=model_file_path,
+                    ctx=ctx,
+                    receptive_field_list=cfg.param_receptive_field_list,
+                    receptive_field_stride=cfg.param_receptive_field_stride,
+                    bbox_small_list=cfg.param_bbox_small_list,
+                    bbox_large_list=cfg.param_bbox_large_list,
+                    receptive_field_center_start=cfg.param_receptive_field_center_start,
+                    num_output_scales=cfg.param_num_output_scales)
 
 class DetectFaces():
     def __init__(self, frame = None):
         ### Loading Face Detection Models
         self.frame = frame
-        model_file_path = "LFFD/models/train_10_320_20L_5scales_v2_iter_1000000.params" 
-        symbol_file_path = "LFFD/symbols/symbol_10_320_20L_5scales_v2_deploy.json"
-        ctx = mx.gpu(0)
+        self.stopped = False
+       
 
-        ### Threshold for Face Matching
-        cosine_threshold = 0.8
-        proba_threshold = 0.6
-        comparing_num = 5
-        face_predictor = predict.Predict(mxnet=mx,
-                                symbol_file_path=symbol_file_path,
-                                model_file_path=model_file_path,
-                                ctx=ctx,
-                                receptive_field_list=cfg.param_receptive_field_list,
-                                receptive_field_stride=cfg.param_receptive_field_stride,
-                                bbox_small_list=cfg.param_bbox_small_list,
-                                bbox_large_list=cfg.param_bbox_large_list,
-                                receptive_field_center_start=cfg.param_receptive_field_center_start,
-                                num_output_scales=cfg.param_num_output_scales)
+    def start(self):
+        print("START VIDEO PROCESSSING")
+        Thread(target=self.drawBBoxes, args=()).start()
+        Thread.daemon = True
+        return self
 
     def drawBBoxes(self):
-        print("Getting Bounding Boxes")
+        while not self.stopped:
+            frame = self.frame
+            bboxes, infer_time = face_predictor.predict(frame, resize_scale=1, score_threshold=0.6, top_k=10000, \
+                                                           NMS_threshold=0.4, NMS_flag=True, skip_scale_branch_list=[]) 
+            
+            h, w, c = frame.shape
+            for bbox in bboxes:
+                print(bbox)
+                bbox_int = [int(b) for b in bbox[:-1]]
+                bbox_int[0] = max(0, min(w - 1, bbox_int[0]))
+                bbox_int[1] = max(0, min(h - 1, bbox_int[1]))
+                bbox_int[2] = max(0, min(w - 1, bbox_int[2]))
+                bbox_int[3] = max(0, min(h - 1, bbox_int[3]))
+                cv2.rectangle(frame, tuple(bbox_int[0:2]), tuple(bbox_int[2:4]), (0, 255, 0), 2)
+                self.frame = frame
+    
+    def stop(self):
+        self.stopped = True
--- start.py
+++ start.py
@@ -1,12 +1,12 @@
 import concurrent.futures
 from camera.camera import VideoGet, VideoShow
-from ultralight.recognize import Recognize
+# from ultralight.recognize import Recognize
 from LFFD.detect import DetectFaces
 
 sources = [
-   'rtsp://QDT:QdtVtx@2020@10.61.166.15/profile3/media.smp',
-   'rtsp://QDT:QdtVtx@2020@10.61.166.135/profile3/media.smp',
-   'rtsp://QDT:QdtVtx@2020@10.61.166.29/profile3/media.smp',
+   # 'rtsp://QDT:QdtVtx@2020@10.61.166.135/profile3/media.smp',
+   # 'rtsp://QDT:QdtVtx@2020@10.61.166.29/profile3/media.smp',
+   'rtsp://QDT:QdtVtx@2020@10.61.166.15/profile3/media.smp'
 ]
 
 MAX_PROCESSES = 3
@@ -14,19 +14,19 @@ MAX_PROCESSES = 3
 class Streaming():
     def __init__(self):
         self.name = "Face ID Project"
-        self.face_detector = DetectFaces()
-    
+
     def processVideo(self, source):
         video_getter = VideoGet(source).start()
         video_shower = VideoShow(video_getter.frame).start()
+        video_processor = DetectFaces(video_getter.frame).start()
 
         while True:
             if video_getter.stopped or video_shower.stopped:
                 video_shower.stop()
                 video_getter.stop()
                 break
-            frame = video_getter.frame
-            video_shower.frame = frame
+            processing_frame = video_processor.frame
+            video_shower.frame = processing_frame
 
     def start_stream(self, streams: list):
     	print("Starting Streams from", streams)
